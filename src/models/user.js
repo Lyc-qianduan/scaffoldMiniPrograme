73c31ab447d2b6f400112a452b6fbfdb5ffb514c
王相博 <7685139+pythonistaWong@user.noreply.gitee.com>
2020-12-02 15:47:10 +0800

    fix: format request fucntion

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-12-02 14:34:39
- * @LastEditTime: 2020-12-02 15:00:45
+ * @LastEditTime: 2020-12-02 15:45:38
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /mpx-tpl/src/models/user.js
@@ -9,13 +9,58 @@
 
 import { Http } from './../utils/requestClass.js'
 
+const isGetMethod = 'get'
+
 class BookModel extends Http {
-  getUser(data = {}) {
-    const method = 'get'
+  getHotList(data = {}) {
     return this.request(
       '/book/hot_list',
       data,
-      method
+      isGetMethod
+    )
+  }
+
+  search(start, q) {
+    return this.request(
+      'book/search?summary=1',
+      {
+        q,
+        start
+      }
+    )
+  }
+
+  getMyBookCount() {
+    return this.request(
+      '/book/favor/count'
+    )
+  }
+
+  getDetail(bid) {
+    return this.request(
+      `book/${bid}/detail`
+    )
+  }
+
+  getLikeStatus(bid) {
+    return this.request(
+      `/book/${bid}/favor`
+    )
+  }
+
+  getComments(bid) {
+    return this.request(
+      `book/${bid}/short_comment`
+    )
+  }
+
+  postComment(bid, comment) {
+    return this.request(
+      'book/add/short_comment',
+      {
+        book_id: bid,
+        content: comment
+      }
     )
   }
 }
